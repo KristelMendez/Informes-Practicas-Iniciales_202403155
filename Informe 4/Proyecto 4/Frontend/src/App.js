fa6cbdc2a79385d090c64fe3688d573cfbded075
Kristel Amarilis Méndez Marroquin <2822557610101@ingenieria.usac.edu.gt>
2026-03-24 22:07:53 -0600

    Paginas de la web

--- Informe 4/Proyecto 4/Frontend/src/App.js
+++ Informe 4/Proyecto 4/Frontend/src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Importamos las páginas que se mostrarán en la aplicación
 import Login from "./pages/Login";
 import Registro from "./pages/Registro";
 import Home from "./pages/Home";
 import CrearPublicacion from "./pages/CrearPublicacion";
 import Perfil from "./pages/Perfil";
+import Recuperar from "./pages/Recuperar";
 
+// Componente principal de la aplicación que define las rutas para cada página
 function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/home" element={<Home />} />
         <Route path="/crear" element={<CrearPublicacion />} />
         <Route path="/perfil" element={<Perfil />} />
+        <Route path="/recuperar" element={<Recuperar />} />
+
       </Routes>
     </Router>
   );
